c3782f14e3eabea6b4d7495c827e2244bd8e9078
uche mbaka <uchembaka@gmail.com>
2025-06-18 21:09:38 +0100

    Null eval_pts handling + internal evals

--- R/eval_mGSFPCA.R
+++ R/eval_mGSFPCA.R
@@ -17,22 +17,29 @@
 #'
 #' @import fda
 #' @export
-eval_mGSFPCA <- function(mGSFPCA_obj, eval_pts) {
-  eval_pts <- sort(eval_pts)
+eval_mGSFPCA <- function(mGSFPCA_obj, eval_pts = NULL) {
+
   k <- mGSFPCA_obj$pars$k
   p <- mGSFPCA_obj$pars$p
-  
-  aT <- mGSFPCA_obj$pars$range[1]; bT <- mGSFPCA_obj$pars$range[2]
-  if (all(eval_pts >= aT) && all(eval_pts <= bT)) {
-    eval_pts <- (eval_pts - aT) / (bT - aT)
-  } else {
-    stop("eval_pts outside of observation point range")
+
+  if (is.null(eval_pts)) {
+    eval_pts <- mGSFPCA_obj$pars$evalGrid
+  } else if (all(eval_pts == mGSFPCA_obj$pars$workGrid)) {
+    eval_pts <- mGSFPCA_obj$pars$workGrid
+  } else { # for external input
+    eval_pts <- sort(eval_pts)
+    aT <- mGSFPCA_obj$pars$range[1]; bT <- mGSFPCA_obj$pars$range[2]
+    if (all(eval_pts >= aT) && all(eval_pts <= bT)) {
+      eval_pts <- (eval_pts - aT) / (bT - aT)
+    } else {
+      stop("eval_pts outside of observation point range")
+    }
   }
-  
+
   cvec <- mGSFPCA_obj$pars$coeffs
   basis <- mGSFPCA_obj$pars$eigBasis
-  npts <- length(eval_pts) 
-  
+  npts <- length(eval_pts)
+
   if (npts > p) {
     Phi <- get_UDUT(cvec, basis, eval_pts, p, k)$U*sqrt(npts)
   } else {
@@ -43,12 +50,12 @@ eval_mGSFPCA <- function(mGSFPCA_obj, eval_pts) {
     R.inv <- solve(chol(BtB))
     B_orth <- (B) %*% R.inv
     beta <- solve((t(B_orth)%*%(B_orth))) %*% t(B_orth) %*% U
-    
+
     eval_B <- fda::eval.basis(eval_pts, basis)
     eval_B_orth <- eval_B %*% R.inv
     Phi <- eval_B_orth %*% beta
-    
+
   }
-  
+
   return(Phi)
 }
